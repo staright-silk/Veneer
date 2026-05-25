ecd0f90fe50f1846f87dbd77ebb2aaac3c0e07e5
Atreya Chakraborty <atreya.sept@gmail.com>
2026-05-25 20:34:38 +0530

    added occupations

--- hello.py
+++ hello.py
@@ -1,4 +1,8 @@
-userdetails=str(input("Enter your name: "))
-print(f"Hello,{userdetails.capitalize()}!")
-print("")
-occupation=str(input)
+name=str(input("Enter your name: "))
+print(f"Hello,{name.capitalize()}!")
+print("You can choose 4 paths / occupations:\n"
+      "1. Carpenter\n"
+      "2. Doctor\n"
+      "3. Fisherman\n"
+      "4. Hunter")
+occupation=str (input("Enter your choice of occupation: ".capitalize()))
